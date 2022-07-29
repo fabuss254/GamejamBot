93b08adeaa9ed1f7da01495eb7911e990f1bbf05
fabuss254 <26258237+fabuss254@users.noreply.github.com>
2022-07-29 14:07:16 +0200

    Update submit.js

--- Commands/submit.js
+++ Commands/submit.js
@@ -11,7 +11,7 @@ Command.Execute = function(Main, msg){
     if (Main.Data[msg.author.id] == undefined) return msg.channel.send("You're not participating in the gamejam yet !\n use ``-participate`` to participate now !");
     if (Main.Data[msg.author.id].Leader != undefined) return msg.channel.send("You're not the leader of your current team. only the leader can submit the game !");
 
-    let Submitted = msg.content.substring(8)
+    let Submitted = msg.content.substring(8).replaceAll("`", "");
     if (msg.mentions.members.size > 0 || msg.mentions.everyone) return msg.channel.send("Submission cannot contain mentions.");
     if (Submitted == undefined || Submitted == "") return msg.channel.send("Submission is invalid.");
     if (Submitted.length > 500) return msg.channel.send("Submission too long. (500 char limit)");
